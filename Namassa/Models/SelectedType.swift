ee87819090efbcfcb5d61b5ffbd74e1f33b79757
Levi Faleh <161657195+LeviFaleh@users.noreply.github.com>
2025-02-28 11:50:58 -0300

    Update SelectedType.swift

--- Namassa/Models/SelectedType.swift
+++ Namassa/Models/SelectedType.swift
@@ -8,16 +8,33 @@
 import Foundation
 
 enum SelectedType {
+    case meat
+    case poultry
+    case pork
+    case fish
+    case vegan
+    case seafood
+    
     case bread
     case rice
     case pasta
     case potato
     case cereals
-    case banana
+    case flour
+
+    case nuts
+    case leafs
+    case roots
+    case fruits
+    case seeds
+    case stems
+    
     case none
-    case all
+    case allCarbs
+    case allProteins
     
     func getOptions() -> [String] {
+        
         switch self {
         case .bread:
             return ["Pão Francês", "Fast"]
@@ -29,11 +46,11 @@ enum SelectedType {
             return ["Batata doce", "Batata inglesa"]
         case .cereals:
             return ["Lentilha", "Chia"]
-        case .banana:
-            return ["Banana prata", "Banana da terra"]
+        case .flour:
+            return ["Farinha de Trigo", "Farinha de Mandioca"]
         case .none:
             return []
-        case .all:
+        case .allCarbs:
             return [
                 "Pão Francês", "Fast",
                 "Arroz branco", "Arroz parboilizado",
@@ -43,6 +60,39 @@ enum SelectedType {
                 "Banana prata", "Banana da terra"
                 
             ]
+        case .meat:
+            return ["Carne Moída", "Filé Mignon", "Contra-filé"]
+        case .poultry:
+            return ["Coxa de Frango", "Sobrecoxa de Frango", "Peito de Pato", "Ovo"]
+        case .pork:
+            return ["Copalombo", "Lombo", "Bacon", "Panceta"]
+        case .fish:
+            return ["Pargo", "Pirarucu", "Tilápia", "Sardinha"]
+        case .vegan:
+            return ["Tofu", "Proteína de Soja"]
+        case .seafood:
+            return ["Camarão", "Lagosta", "Ostra", "Mexilhão"]
+        case .allProteins:
+            return ["Carne Moída", "Filé Mignon", "Contra-filé",
+                    "Coxa de Frango", "Sobrecoxa de Frango", "Peito de Pato",
+                    "Copalombo", "Lombo", "Bacon", "Panceta",
+                    "Pargo", "Pirarucu", "Tilápia", "Sardinha",
+                    "Tofu", "Proteína de Soja",
+                    "Camarão", "Lagosta", "Ostra", "Mexilhão"
+            ]
+        case .nuts:
+            return ["Castanha do Pará", "Castanha de Caju", "Amêndoa"]
+        case .leafs:
+            return ["Rúcula", "Alface", "Acelga", "Repolho"]
+        case .roots:
+            return ["Cenoura", "Rabanete", "Inhame"]
+        case .fruits:
+            return ["Maçã", "Laranja"]
+        case .seeds:
+            return ["Linhaça", "Gergelim"]
+        case .stems:
+            return ["Palmito Pupunha"]
         }
     }
 }
+
