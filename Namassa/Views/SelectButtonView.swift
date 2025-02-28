1a8a7f5cf376756c4af22a53b1ccea9620b26eb7
Levi Faleh <161657195+LeviFaleh@users.noreply.github.com>
2025-02-28 11:55:50 -0300

    Update SelectButtonView.swift

--- Namassa/Views/SelectButtonView.swift
+++ Namassa/Views/SelectButtonView.swift
@@ -8,11 +8,25 @@
 import SwiftUI
 
 struct SelectButtonView: View {
+    var buttonSelected: Bool
+//    @Binding var buttonSelected: Bool
+    @State var textColor: Color
+    @State var color: Color
+    @State var text: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            RoundedRectangle(cornerRadius: 10)
+                .frame(width: 113, height: 61)
+                .foregroundColor(buttonSelected ? color : Color(hex:"CDCDCD"))
+            Text(text)
+                .fontWeight(.medium)
+                .font(.system(size: 24))
+                .foregroundColor(buttonSelected ? textColor : .black)
+        }
     }
 }
 
 #Preview {
-    SelectButtonView()
+    SelectButtonView(buttonSelected: false, textColor: .white, color: .red, text: "pec")
 }
