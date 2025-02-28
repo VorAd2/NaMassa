33e8b8dd8546c1b49094720f1f219acf137f4827
Levi Faleh <161657195+LeviFaleh@users.noreply.github.com>
2025-02-28 11:50:34 -0300

    Update RecipeViewModel.swift

--- Namassa/Models/RecipeViewModel.swift
+++ Namassa/Models/RecipeViewModel.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+import SwiftUI
+
+class RecipeViewModel: ObservableObject {
+    
+    @Published var allRecipes = [RecipeModel]()
+   var dbData = [RecipeModel]()
+    init() {
+        allRecipes = loadJsonFile()
+    }
+    
+    private func loadJsonFile() -> [RecipeModel]{
+        do {
+            if let filePath  = Bundle.main.url(forResource: "RecipesDataBase", withExtension: "json") {
+                
+                let jsonData = try Data(contentsOf: filePath)
+                let decodedData = try JSONDecoder().decode([RecipeModel].self, from: jsonData)
+                print(decodedData)
+                dbData = decodedData
+            } else {
+                return [RecipeModel]()
+            }
+        } catch let error {
+            print("[System] Error while fetching: \(error)")
+        }
+        return dbData
+    }
+}
