e2c9588511bac95a9cd9188b3197fe35251178b1
Levi Faleh <161657195+LeviFaleh@users.noreply.github.com>
2025-02-28 11:51:15 -0300

    Update ViewModel.swift

--- Namassa/Models/ViewModel.swift
+++ Namassa/Models/ViewModel.swift
@@ -8,5 +8,12 @@
 import Foundation
 
 class ViewModel: ObservableObject {
-    @Published var tags = []
+    
+    @Published var tags: [String] = []
+    @Published var selectedCarbType: SelectedType = .none
+    @Published var selectedProteinType: SelectedType = .none
+    @Published var selectedVeggieType: SelectedType = .none
+    @Published var recipe: RecipeModel = .init(id: 0, image: "", ingredients: [], name: "", tags: [])
+    @Published var goToRecipe: Bool = false
+//    @Published var recipeViewModel = RecipeViewModel()
 }
